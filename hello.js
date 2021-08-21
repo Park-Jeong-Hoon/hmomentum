492a90a7d8effb2c9fd8ed98d290bae9bb27a4a3
Park-Jeong-Hoon <77821198+Park-Jeong-Hoon@users.noreply.github.com>
2021-08-21 17:01:47 +0900

    Add hello.js, time.js, style.css

---
+++ hello.js
@@ -0,0 +1,26 @@
+const loginInput = document.querySelector("#login input:first-child");
+const loginForm = document.querySelector("#login");
+const username = document.querySelector("#username");
+
+const USER_NAME = localStorage.getItem("username");
+
+function drawName(name) {
+    loginForm.classList.add("hide");
+    username.classList.remove("hide");
+    username.innerText = name;
+}
+
+if(USER_NAME === null) {
+    loginForm.classList.remove("hide");
+} else {
+    drawName(USER_NAME);
+}
+
+function submitEvent(event) {
+    event.preventDefault();
+    const loginName = loginInput.value;
+    localStorage.setItem("username", loginName);
+    drawName(loginName);
+}
+
+loginForm.addEventListener("submit", submitEvent);
---
+++ time.js
@@ -0,0 +1,15 @@
+const time = document.querySelector("#time");
+
+function nowTime() {
+    const date = new Date(); //현재 시간에 대한 정보를 가져올 수 있다.
+    /*padStart를 이용하면 padStart의 첫번째 인자 만큼 출력되게 하면서 
+    만약 글자가 모자라면 padStart의 두번째 인자로 모자란 만큼의 앞부분을 채워준다.
+    그런데 padStart는 string형 변수에만 사용가능해서 string형이 아닌 경우에는 형변환을 해주면 된다.*/
+    const hours = String(date.getHours()).padStart(2, "0"); //시
+    const minutes = String(date.getMinutes()).padStart(2, "0"); //분
+    const seconds = String(date.getSeconds()).padStart(2, "0"); //초
+    time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+nowTime();
+setInterval(nowTime, 1000); //1000밀리초의 간격을 두고 nowTime함수가 실행된다.
